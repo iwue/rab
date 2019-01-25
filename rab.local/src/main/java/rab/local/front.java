45abb5093bd06e3e4f5d907744e48eda88a323ac
Pascal <Pascal@DESKTOP-1PJ4OGH>
2019-01-25 17:03:00 +0100

    chose front erstellt

--- rab.local/src/main/java/rab/local/front.java
+++ rab.local/src/main/java/rab/local/front.java
@@ -7,10 +7,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class front extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField Y;
+	private JTextField Z;
+	private JTextField X;
 
 	/**
 	 * Launch the application.
@@ -39,6 +47,54 @@ public class front extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("RAB project \"ebe\"");
+		label.setBounds(10, 11, 152, 31);
+		label.setForeground(Color.CYAN);
+		label.setFont(new Font("Yu Gothic", Font.BOLD | Font.ITALIC, 14));
+		contentPane.add(label);
+		
+		Y = new JTextField();
+		Y.setBounds(56, 80, 106, 20);
+		contentPane.add(Y);
+		Y.setColumns(10);
+		
+		Z = new JTextField();
+		Z.setBounds(56, 163, 106, 20);
+		contentPane.add(Z);
+		Z.setColumns(10);
+		
+		X = new JTextField();
+		X.setBounds(251, 163, 106, 20);
+		contentPane.add(X);
+		X.setColumns(10);
+		
+		JLabel Ycoordinate = new JLabel("Y-Coordinate");
+		Ycoordinate.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		Ycoordinate.setForeground(Color.GREEN);
+		Ycoordinate.setBounds(56, 53, 87, 16);
+		contentPane.add(Ycoordinate);
+		
+		JLabel Zcoordinates = new JLabel("Z-Coordinate");
+		Zcoordinates.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		Zcoordinates.setForeground(Color.GREEN);
+		Zcoordinates.setBounds(56, 138, 106, 14);
+		contentPane.add(Zcoordinates);
+		
+		JLabel Xcoordinates = new JLabel("X-Coordinate");
+		Xcoordinates.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		Xcoordinates.setForeground(Color.GREEN);
+		Xcoordinates.setBounds(251, 138, 106, 14);
+		contentPane.add(Xcoordinates);
+		
+		JButton btnNewButton = new JButton("Run");
+		btnNewButton.setBounds(300, 215, 91, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel choose = new JLabel("choose the position");
+		choose.setFont(new Font("Yu Gothic", Font.BOLD | Font.ITALIC, 16));
+		choose.setForeground(Color.CYAN);
+		choose.setBounds(230, 53, 161, 23);
+		contentPane.add(choose);
 	}
-
 }
