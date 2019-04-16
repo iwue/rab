e54503c39ce3b5f34dae9fa936b60dfc0efe25f3
IsaacWuerth <isaac@it-wuerth.ch>
2019-04-16 23:45:41 +0200

    Fehler angepasst

--- rab.local/src/main/java/local/rab/controller/calculation/CalculateAngelsToCoordinate.java
+++ rab.local/src/main/java/local/rab/controller/calculation/CalculateAngelsToCoordinate.java
@@ -12,7 +12,7 @@ public class CalculateAngelsToCoordinate {
 	
 	public Point3D calc(BrickComponentHandler componentHandler) {
 		try {
-			double angleTheta1 = componentHandler.getHingTheta1().getTachoCount() * Statics.getTransmissionTheta1();			
+			double angleTheta1 = componentHandler.getHingTheta1().getTachoCount() * Statics.getTransmissionTheta1();
 			double angleTheta2 = componentHandler.getHingTheta20().getTachoCount() * Statics.getTransmissionTheta2() * -1 + 90;
 			double angleTheta3 = componentHandler.getHingTheta3().getTachoCount() * Statics.getTransmissionTheta3()* -1 - 90;
 			double angleTheta4 = componentHandler.getHingTheta4().getTachoCount() * Statics.getTransmissionTheta4()* -1;
@@ -25,13 +25,18 @@ public class CalculateAngelsToCoordinate {
 	}
 	
 	public Point3D calc(double angleTheta1, double angleTheta2, double angleTheta3, double angleTheta4) {
-		double z = Statics.getA1() 
-				+ (Statics.getA2() * Math.sin(angleTheta2))
-				+ (Statics.getA5() * Math.sin(angleTheta2 + angleTheta3));
+		double a1 = Statics.getA1();
+		double a2 = Statics.getA2();
+		double a5 = Statics.getA5();
+		double a6 = Statics.getA6();
 		
-		double c = (Statics.getA2() * Math.cos(angleTheta2))
-				+ (Statics.getA5() * Math.cos(angleTheta2 + angleTheta3))
-				+ Statics.getA6();
+		double z = a1 
+				+ a2 * Math.sin(Math.toRadians(angleTheta2))
+				+ a5 * Math.sin(Math.toRadians(angleTheta3 + angleTheta2));
+		
+		double c = (a2 * Math.cos(Math.toRadians(angleTheta2)))
+				+ (a5 * Math.cos(Math.toRadians(angleTheta3 + angleTheta2)))
+				+ a6;
 		
 		double y = c * Math.sin(angleTheta1);
 		double x = c * Math.cos(angleTheta1);
