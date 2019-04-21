e21ca810801294993bedbeb1a97ae487e8e54620
IsaacWuerth <isaac@it-wuerth.ch>
2019-04-21 19:09:51 +0200

    InfoFront ausversehen gelöscht, IDPA Daten hinzugefügt als 3 Modus

---
+++ rab.local/src/main/java/local/rab/old/controller/RabMainController.java
@@ -0,0 +1,95 @@
+package local.rab.old.controller;
+
+import java.util.LinkedList;
+
+import javafx.geometry.Point3D;
+import local.rab.config.Statics;
+import local.rab.devices.dualshock.DualshockSimple;
+
+public class RabMainController {
+	private BrickMoveController moveBrickController;
+	private DualshockSimple dualshockSimple;
+
+	public RabMainController(DualshockSimple dualshockSimple,
+			BrickMoveController moveBrickController) {
+		try {
+			this.dualshockSimple = dualshockSimple;
+
+			this.moveBrickController = moveBrickController;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void choreography(LinkedList<Point3D>  point3ds, double timeForMove) {
+		while (!point3ds.isEmpty()) {
+			moveBrickController.goTo(point3ds.removeFirst(), timeForMove);
+		}
+	}
+
+	/**
+	 * Umrechnung von Koordinatensystem zu Roboterachsen und Ausführen der Bewegung.
+	 */
+	public void move() {
+		// Auslesen der X-Richtung auf dem Joystick
+		double joystickCurrentX = dualshockSimple.getLeftStickX();
+		// Auslesen der Y-Richtung auf dem Joystick
+		double joystickCurrentY = dualshockSimple.getLeftStickY() * -1;
+		// Auslesen der Z-Richtung auf dem Joystick
+		double joystickCurrentZ = dualshockSimple.getRightStickY() * -1;
+
+		// Prüfen ob die Joystickdaten von X, Y und Z sich im Stoppbereich befinden,
+		// weil sich diese Signale nicht genau am 0 Punkt sind
+		if (joystickCurrentX > (Statics.getDualshockStopRange() * -1)
+				&& joystickCurrentX < Statics.getDualshockStopRange()
+				&& joystickCurrentY > (Statics.getDualshockStopRange() * -1)
+				&& joystickCurrentY < Statics.getDualshockStopRange()
+				&& joystickCurrentZ > (Statics.getDualshockStopRange() * -1)
+				&& joystickCurrentZ < Statics.getDualshockStopRange()) {
+
+			// Falls sich alle im Stoppbereich befinden,
+			// werden alle Motoren gestoppt
+			moveBrickController.stopAllAngels();
+
+		} else {
+			// Berechnung der Bewegung in X-Richtung
+			double moveX = Statics.getMaxSpeedOnCoordinateSystem() * joystickCurrentX;
+			// Berechnung der Bewegung in Y-Richtung
+			double moveY = Statics.getMaxSpeedOnCoordinateSystem() * joystickCurrentY;
+			// Berechnung der Bewegung in Z-Richtung
+			double moveZ = Statics.getMaxSpeedOnCoordinateSystem() * joystickCurrentZ;
+
+			// Addieren der Bewegung zur Akuellen Position
+			// Es ensteht die neue Position
+			double newX = Statics.getCurrentX() + moveX;
+			double newY = Statics.getCurrentY() + moveY;
+			double newZ = Statics.getCurrentZ() + moveZ;
+
+			// Festlegen der Geschwindigkeit für die Achsen
+			if (moveBrickController.setSpeedForAllAngles(newX, newY, newZ)) {
+				// Alle Motoren starten
+				moveBrickController.goAllAngels();
+			}
+
+			try {
+				// Programm pausieren für Bewegung
+				Thread.sleep((int) (1000 * Statics.getInterval()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public BrickMoveController getMoveBrickController() {
+		return moveBrickController;
+	}
+
+	public DualshockSimple getDualshockSimple() {
+		return dualshockSimple;
+	}
+
+	public void setDualshockSimple(DualshockSimple dualshockSimple) {
+		this.dualshockSimple = dualshockSimple;
+	}
+}
---
+++ rab.local/src/main/java/local/rab/windows/InfoFront.java
@@ -0,0 +1,48 @@
+package local.rab.windows;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class InfoFront extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel InfoBox;
+	
+	/**
+	 * Create the frame.
+	 */
+	public InfoFront() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		InfoBox = new JLabel("");
+		InfoBox.setForeground(Color.RED);
+		InfoBox.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		InfoBox.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(InfoBox);
+		setUndecorated(true);
+	}
+
+	public JLabel getInfoBox() {
+		return InfoBox;
+	}
+}
